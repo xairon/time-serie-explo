f4050d24412ec9390ad178cf9cb5594aaa2a4a66
Ben Crulis <bencrulis@gmail.com>
2025-11-17 15:10:57 +0100

    refactored config and argument parsing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+from pathlib import Path
 import sys
 import argparse
 import numpy as np
@@ -8,6 +9,7 @@ from codecarbon import EmissionsTracker
 import torch
 import json
 import time
+import argparse
 
 from tools.gradcam_utils import grad_cam_timeseries
 from tools.plots import save_results, plot_selection_distribution, plot_module_on_series
@@ -15,6 +17,47 @@ from tools.metrics import metric
 
 from exp.exp_main import Exp_Main
 
+
+DEFAULT_CONFIG_PATH = Path("configs/linearModel.json")
+
+#### General parameters ####
+# model         --> DLinear, PatchMixer, PatchTST
+# input_len     --> sequence length
+# pred_len      --> prediction length
+# input_sze     --> number of features (piezo=4)
+# split_ratio   --> train data split ratio (train, val=0.1, test=1-train-val)
+# lr            --> learning rate
+# epochs        --> number of training epochs
+# patience      --> early stopping patience
+# batch_size    --> batch size
+# debug         --> debug mode (True/False)
+# save_name     --> auto name for saving results
+
+#### visualization parameters ####
+# tracking_gradcam  --> whether to track gradcam during testing
+# attn_topk       --> top k% of attention to consider for visualization
+
+#### Mixer Model parameters ####
+# label_len     --> start token length
+# enc_in        --> encoder input size
+# seq_len       --> input sequence length
+# patch_len     --> patch length
+# stride        --> patch stride
+# mixer_kernel_size  --> mixer layer kernel size
+# d_model       --> dimension of model
+# dropout       --> dropout rate
+# head_dropout  --> dropout rate last
+# e_layers      --> number of encoder layers
+
+#### PatchTST Model parameters ####
+# fc_dropout    --> dropout rate for the final fully connected layer
+# padding_patch --> whether to pad the last patch if needed (end)
+# revin         --> whether to use RevIN normalization
+# affine        --> whether to use affine transformation in RevIN
+# substract_last--> whether to substract the last value in the series for trend modeling
+# decomposition --> whether to use series decomposition block
+# kernel_size   --> convolutional kernel size for series decomposition
+# d_ff          --> dimension of feedforward layer
 
 #set Seed
 def set_seed(seed=42):
@@ -25,56 +68,31 @@ def set_seed(seed=42):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-def make_parser(args_dict):
+
+def make_parser():
     parser = argparse.ArgumentParser(description="Models args")
-    for key, value in args_dict.items():
-        parser.add_argument(f'--{key}', type=type(value), default=value)
-    args = parser.parse_args([])
+    parser.add_argument("--config", type=Path, default=DEFAULT_CONFIG_PATH, help="config file path")
+    parser.add_argument("--seed", type=int, default=42, help="random seed")
+
+    known_args = parser.parse_known_args()[0]
+
+    current_config_path = known_args.config
+
+    current_config = {}
+    with open(current_config_path, 'r') as file:
+        current_config = json.load(file)
+
+    for key, value in current_config.items():
+        # if key not in parser arguments, add it
+        if not any(arg.dest == key for arg in parser._actions):
+            parser.add_argument(f'--{key}', type=type(value), default=value)
+
+    # set defaults again
+    parser.set_defaults(**current_config)
+
+    args = parser.parse_args()
     return args
 
-config = {}
-
-# Open Arguments file
-with open('config.json', 'r') as file:
-    config = json.load(file)
-    #### General parameters ####
-    # model         --> DLinear, PatchMixer, PatchTST
-    # input_len     --> sequence length
-    # pred_len      --> prediction length
-    # input_sze     --> number of features (piezo=4)
-    # split_ratio   --> train data split ratio (train, val=0.1, test=1-train-val)
-    # lr            --> learning rate
-    # epochs        --> number of training epochs
-    # patience      --> early stopping patience
-    # batch_size    --> batch size
-    # debug         --> debug mode (True/False)
-    # save_name     --> auto name for saving results
-
-    #### visualization parameters ####
-    # tracking_gradcam  --> whether to track gradcam during testing
-    # attn_topk       --> top k% of attention to consider for visualization
-
-    #### Mixer Model parameters ####
-    # label_len     --> start token length
-    # enc_in        --> encoder input size
-    # seq_len       --> input sequence length
-    # patch_len     --> patch length
-    # stride        --> patch stride
-    # mixer_kernel_size  --> mixer layer kernel size
-    # d_model       --> dimension of model
-    # dropout       --> dropout rate
-    # head_dropout  --> dropout rate last
-    # e_layers      --> number of encoder layers
-
-    #### PatchTST Model parameters ####
-    # fc_dropout    --> dropout rate for the final fully connected layer
-    # padding_patch --> whether to pad the last patch if needed (end)
-    # revin         --> whether to use RevIN normalization
-    # affine        --> whether to use affine transformation in RevIN
-    # substract_last--> whether to substract the last value in the series for trend modeling
-    # decomposition --> whether to use series decomposition block
-    # kernel_size   --> convolutional kernel size for series decomposition
-    # d_ff          --> dimension of feedforward layer
 
 def main(args, file):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -128,9 +146,14 @@ def main(args, file):
     return metrics_test, metrics_val
 
 if __name__ == '__main__':
-    set_seed(42)
+    args = make_parser()
 
-    args = make_parser(config)
+    print("Arguments:")
+    for arg in vars(args):
+        print(f"  {arg}: {getattr(args, arg)}")
+    print()
+
+    set_seed(args.seed)
 
     tracker = EmissionsTracker() #allow_multiple_runs=True
     tracker.start()
